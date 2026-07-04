59fb3a1efe389aad389aa5dc81929ce305b85f4f
AbdelkaderBarhoumi21 <abdelkaderbarhoumi21@gmail.com>
2026-07-04 15:07:51 +0100

    feat: add centralized UI string constants

---
+++ app/src/main/java/com/example/pulse_app/utils/AppConstants.kt
@@ -0,0 +1,8 @@
+package com.example.pulse_app.utils
+
+object AppConstants {
+    const val DATABASE_NAME= "task_pulse.db"
+    const val SYNC_WORK_NAME= "task_sync"
+    const val DEVICE_PLATFORM = "android"
+    const val NETWORK_TIMEOUT_SECONDS = 30L // Int is 32 bit and Long is 64 bit
+}
