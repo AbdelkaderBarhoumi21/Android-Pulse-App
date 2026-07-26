b1aec0b17af230a60614ed5a27ddbb0492a82526
AbdelkaderBarhoumi21 <abdelkaderbarhoumi21@gmail.com>
2026-07-26 15:01:27 +0100

    refactor: update Task to TaskModel

--- app/src/main/java/com/example/pulse_app/features/task/domain/model/TaskModel.kt
+++ app/src/main/java/com/example/pulse_app/features/task/domain/model/TaskModel.kt
@@ -1,7 +1,7 @@
 package com.example.pulse_app.features.task.domain.model
 import java.time.Instant
 
-data class Task(
+data class TaskModel(
     val id: String,
     val title: String,
     val description: String,
