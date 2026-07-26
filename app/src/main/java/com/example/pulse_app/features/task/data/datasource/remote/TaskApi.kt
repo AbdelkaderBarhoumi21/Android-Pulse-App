23f64a07540cad7691f9c970e7867af1e7ff10b8
AbdelkaderBarhoumi21 <abdelkaderbarhoumi21@gmail.com>
2026-07-26 13:04:58 +0100

    feat: add task api interface

---
+++ app/src/main/java/com/example/pulse_app/features/task/data/datasource/remote/TaskApi.kt
@@ -0,0 +1,30 @@
+package com.example.pulse_app.features.task.data.datasource.remote
+
+import androidx.room.Delete
+import com.example.pulse_app.core.network.ApiEndpoints
+import com.example.pulse_app.features.task.data.datasource.remote.dto.CreateTaskRequestDto
+import com.example.pulse_app.features.task.data.datasource.remote.dto.TaskDto
+import com.example.pulse_app.features.task.data.datasource.remote.dto.UpdateTasRequestDto
+import retrofit2.http.*
+
+interface TaskApi{
+    /// Get all tasks from server.
+    @GET(ApiEndpoints.TASKS)
+    suspend fun getTasks():List<TaskDto>
+
+    /// Get task from server
+    @GET(ApiEndpoints.TASK_BY_ID)
+    suspend fun getTask(@Path(ApiEndpoints.PATH_ID) id: String): TaskDto
+
+    /// Create new task
+    @POST(ApiEndpoints.TASKS)
+    suspend fun createTask(@Body body: CreateTaskRequestDto): TaskDto
+
+    /// Update task
+    @PUT(ApiEndpoints.TASK_BY_ID)
+    suspend fun updateTask(@Path(ApiEndpoints.PATH_ID) id :String,@Body body: UpdateTasRequestDto): TaskDto
+
+    /// Delete task
+    @DELETE(ApiEndpoints.TASK_BY_ID)
+    suspend fun deleteTask(@Path(ApiEndpoints.PATH_ID) id: String)
+}
