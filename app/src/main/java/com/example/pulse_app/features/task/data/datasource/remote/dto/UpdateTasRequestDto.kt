b10844c5055e1110c2025362eef23b87ed2ebb51
AbdelkaderBarhoumi21 <abdelkaderbarhoumi21@gmail.com>
2026-07-29 17:26:16 +0100

    refactor: refactor update request dto name and nullable fields

--- app/src/main/java/com/example/pulse_app/features/task/data/datasource/remote/dto/UpdateTasRequestDto.kt
+++ app/src/main/java/com/example/pulse_app/features/task/data/datasource/remote/dto/UpdateTasRequestDto.kt
@@ -3,9 +3,9 @@ package com.example.pulse_app.features.task.data.datasource.remote.dto
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class UpdateTasRequestDto(
-    val title:String,
-    val description:String,
-    val priority:String,
-    val status:String?=null,
+data class UpdateTaskRequestDto(
+    val title: String? = null,
+    val description: String? = null,
+    val priority: String? = null,
+    val status: String? = null,
 )
